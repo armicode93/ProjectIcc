ee180319f186d1c9b549357888eaedc5e3a3a1ae
naser <beab0907@student.iepscf-uccle.be>
2021-03-24 10:25:59 +0100

    initial commit

---
+++ src/main/java/application/demo/ProjectIntegrationAppApplication.java
@@ -0,0 +1,13 @@
+package application.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIntegrationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectIntegrationAppApplication.class, args);
+    }
+
+}
